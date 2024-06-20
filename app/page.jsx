e1c9ff42a8a0e4a41cc277e18c540e39e60dddab
Camila <camilagomes_gc@hotmail.com>
2024-06-20 17:03:19 +0200

    Fix component name

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Hero from "@/components/Hero";
-import InfoBoxes from "@/components/InfoBxes";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
   return (
